6592a6bd0c0293e9b80e77dd0592f23b8cb18b57
aladynjr <aladynjr@gmail.com>
2023-02-13 03:11:11 +0100

    add guide part in landing page for desktop view

---
+++ src/components/AlertDemoSection.js
@@ -0,0 +1,76 @@
+import React from 'react'
+
+function AlertDemoSection() {
+  return (
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-around', width: '90%', maxWidth: '1300px', margin: 'auto', paddingTop: '60px' }} className='flex-col-reverse xl:flex-row alert-section-mobile' >
+
+
+    <div className='block p-6 rounded-lg shadow-lg xl:shadow-2xl bg-white' style={{ width: '90%', maxWidth: '450px', margin: 'auto' }}>
+      <div style={{ margin: 'auto' }}>
+        <div className="mb-3 ">
+          <label
+            className="form-label alert-form-select-label inline-block mb-2 text-gray-700 text-sm"
+          >Course</label>
+          <select className="form-select alert-form-select appearance-none block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="Default select example">
+            <option selected>Select your course</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </select>
+        </div>
+        <div className="mb-3 ">
+          <label
+            className="form-label alert-form-select-label inline-block mb-2 text-gray-700 text-sm"
+          >Date</label>
+          <select className="form-select alert-form-select appearance-none block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="Default select example">
+            <option selected>Select Date</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </select>
+        </div>
+        <div className="mb-3 ">
+          <label
+            className="form-label alert-form-select-label inline-block mb-2 text-gray-700 text-sm"
+          >Time</label>
+          <select className="form-select alert-form-select appearance-none block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="Default select example">
+            <option selected>Select Time</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </select>
+        </div>
+        <div className="mb-3 ">
+          <label
+            className="form-label alert-form-select-label inline-block mb-2 text-gray-700 text-sm"
+          >Group Size</label>
+          <select className="form-select alert-form-select appearance-none block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="Default select example">
+            <option selected>Select Group Size</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </select>
+        </div>
+
+        <button
+          type="button"
+          data-mdb-ripple="true"
+          data-mdb-ripple-color="light"
+          className="inline-block px-6 py-2.5  text-white font-medium text-xs leading-tight bg-green-500 uppercase rounded  hover:bg-green-600 hover:shadow-lg  focus:shadow-lg 
+          focus:outline-none focus:ring-0  active:shadow-lg transition duration-150 ease-in-out  hero-button-mobile"
+          style={{ padding: '14px 28px', width: '202px', height: '52px', borderRadius: '12px', color: '#F5F8FD', fontSize: '16px', fontWeight: '700', lineHeight: '150%', marginTop: '25px' }}
+        >SET UP ALERT NOW</button>
+
+      </div>
+    </div>
+
+    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-around', height: '250px', width: '90%', maxWidth: '550px' }} >
+      <div className=' alert-title ' > Create an Alert </div>
+      <div className='alert-description'  >Our software constantly crawls golf booking sites looking for cancellations. Once a tee time opens up that meets your criteria, we will send you an email and/or text with a link to book.</div>
+
+    </div>
+
+  </div>  )
+}
+
+export default AlertDemoSection
--- src/pages/Landingpage.js
+++ src/pages/Landingpage.js
@@ -3,85 +3,86 @@ import React, { useEffect } from 'react'
 import { auth } from "../firebase-config";
 import Navbar from '../components/Navbar';
 import HeroSection from '../components/HeroSection';
+import AlertDemoSection from '../components/AlertDemoSection';
+
+import Guide1 from '../assets/images/guide-1.svg'
+import Guide2 from '../assets/images/guide-2.svg'
+import Guide3 from '../assets/images/guide-3.svg'
+import Guide4 from '../assets/images/guide-4.svg'
+
+
 function Landingpage() {
   // RedirectWhenLoggedOrNotLogged( '/home', false)
 
+  const guideSteps = [
+    {
+      title: 'Insert Preferences',
+      description: 'Insert your booking preferences (course, date, time, group size)',
+      image: Guide1
+
+    },
+    {
+      title: 'We’ll Start Looking',
+      description: 'We will constantly crawl the tee sheets at your desired courses waiting for cancellations.',
+      image: Guide2
+    },
+    {
+      title: 'Get Alerted',
+      description: 'Once a tee time becomes available, we’ll text you right away with a link to book.',
+      image: Guide3
+    },
+    {
+      title: 'Book Tee Time',
+      description: 'Most tee times are snagged within minutes so book as quickly as possible.',
+      image: Guide4
+    }
+  ]
 
   return (
     <div style={{ background: '#F5F8FD' }}>
       <Navbar />
       <HeroSection />
       <div style={{ marginBlock: '100px' }}></div>
+      <AlertDemoSection />
+      <div style={{ marginBlock: '100px' }}></div>
 
-      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-around', width: '90%', maxWidth: '2000px', margin: 'auto', paddingTop: '60px' }} className='flex-col-reverse xl:flex-row alert-section-mobile' >
+      <div style={{ height: '50px', background: "white", borderTopRightRadius: "100%", borderTopLeftRadius: '100%' }} ></div>
+
+      <div style={{ background: 'white', paddingBottom:'75px' }} >
+      
+   
+      <div>
+        <div className=' alert-title ' style={{ width: 'fit-content', margin: 'auto', paddingBlock:'75px' }} > How It Works </div>
 
 
-      <div className='block p-6 rounded-lg shadow-lg xl:shadow-2xl bg-white' style={{ width: '90%', maxWidth: '450px', margin: 'auto' }}>
-        <div style={{ margin: 'auto' }}>
-          <div className="mb-3 ">
-            <label
-              className="form-label alert-form-select-label inline-block mb-2 text-gray-700 text-sm"
-            >Course</label>
-            <select className="form-select alert-form-select appearance-none block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="Default select example">
-              <option selected>Select your course</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
-            </select>
-          </div>
-          <div className="mb-3 ">
-            <label
-              className="form-label alert-form-select-label inline-block mb-2 text-gray-700 text-sm"
-            >Date</label>
-            <select className="form-select alert-form-select appearance-none block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="Default select example">
-              <option selected>Select Date</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
-            </select>
-          </div>
-          <div className="mb-3 ">
-            <label
-              className="form-label alert-form-select-label inline-block mb-2 text-gray-700 text-sm"
-            >Time</label>
-            <select className="form-select alert-form-select appearance-none block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="Default select example">
-              <option selected>Select Time</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
-            </select>
-          </div>
-          <div className="mb-3 ">
-            <label
-              className="form-label alert-form-select-label inline-block mb-2 text-gray-700 text-sm"
-            >Group Size</label>
-            <select className="form-select alert-form-select appearance-none block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="Default select example">
-              <option selected>Select Group Size</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
-            </select>
-          </div>
+        <div>
+          {guideSteps.map((step, index) => {
+            return (
+              <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-around', width: '90%', maxWidth: '1400px', margin: 'auto', paddingTop: '60px', flexDirection: ((index + 1) % 2 == 0) && 'row-reverse' }} className='flex-col-reverse xl:flex-row hero-section-mobile' >
+                <img src={step.image} alt="How it works" style={{ width: '90%', maxWidth: '400px', maxHeight: '30%' }} />
 
-          <button
-            type="button"
-            data-mdb-ripple="true"
-            data-mdb-ripple-color="light"
-            className="inline-block px-6 py-2.5  text-white font-medium text-xs leading-tight bg-green-500 uppercase rounded  hover:bg-green-600 hover:shadow-lg  focus:shadow-lg 
-              focus:outline-none focus:ring-0  active:shadow-lg transition duration-150 ease-in-out  hero-button-mobile"
-            style={{ padding: '14px 28px', width: '202px', height: '52px', borderRadius: '12px', color: '#F5F8FD', fontSize: '16px', fontWeight: '700', lineHeight: '150%', marginTop: '25px' }}
-          >SET UP ALERT NOW</button>
+
+                <div>
+                  <div style={{ display: 'flex', alignItems: 'center' }} >
+                    <div className='guide-numbering' style={{width:'95px', marginLeft:'-20px', marginRight:'20px'}} >{'0' + (index + 1)}</div>
+
+                    <div className=' alert-title ' style={{ fontSize: '34px', marginLeft: '-15px', marginBottom: '-15px' }} > {step.title} </div>
+                  </div>
+                  <div className='alert-description' style={{ marginTop: '-22px', marginLeft: '80px', maxWidth: '500px' }}   >{step.description}</div>
+                </div>
+
+
+              </div>
+            )
+          })}
+
 
         </div>
-      </div>
-
-    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-around', height: '250px', width: '90%', maxWidth: '550px' }} >
-        <div className=' alert-title ' > Create an Alert </div>
-        <div className='alert-description'  >Our software constantly crawls golf booking sites looking for cancellations. Once a tee time opens up that meets your criteria, we will send you an email and/or text with a link to book.</div>
+      </div> 
 
       </div>
+      <div style={{ height: '50px', background: "white", borderBottomRightRadius: "100%", borderBottomLeftRadius: '100%' }} ></div>
 
-      </div>
       <div style={{ paddingBottom: '200px' }} ></div>
     </div>
   )
