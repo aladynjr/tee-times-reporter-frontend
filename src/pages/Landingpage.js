539721752c0271012ee055ee37a8b4da61b05c13
aladynjr <aladynjr@gmail.com>
2023-02-14 04:44:17 +0100

    add insights section for desktop and mobile

--- src/pages/Landingpage.js
+++ src/pages/Landingpage.js
@@ -7,9 +7,9 @@ import AlertDemoSection from '../components/AlertDemoSection';
 import GuideSection from '../components/GuideSection';
 import TipsSection from '../components/TipsSection';
 import TestimonialsBG from '../assets/images/testimonials-bg.svg'
-import GolfCart from '../assets/images/golf-cart.svg'
-import GolfCartShadow from '../assets/images/golf-cart-shadow.svg'
-import Logo from '../assets/images/logo.svg'
+import Footer from '../components/Footer';
+import GolfPlayers from '../assets/images/golf-players.svg'
+import Blob from '../assets/images/blob.svg'
 
 function Landingpage() {
   // RedirectWhenLoggedOrNotLogged( '/home', false)
@@ -32,70 +32,37 @@ function Landingpage() {
     <div  className='testimonials' >
     <div className=' alert-title ' style={{ color:'white', width: 'fit-content', margin: 'auto', paddingBlock: '75px' }} > What Golfers Are Saying </div>
     </div> */}
+
+      <div>
+        <div style={{ background: 'white', paddingBlock:'170px', paddingBottom:'70px' }} >
+          <img src={Blob} alt="blob" className='insights-blob'  />
+
+          <div className='insights-wrapper'  >
+            <div>
+              <img src={GolfPlayers} alt="golf players" style={{ width: '90%', maxWidth: '1200px', height: 'auto', position: 'relative', zIndex: '1', margin: 'auto' }} />
+            </div>
+            <div style={{ width: '90%', maxWidth: '700px' }} >
+              <div className=' alert-title ' style={{ color: 'rgba(44, 60, 85, 1)', paddingBlock: '25px' }} > Why This Works </div>
+
+              <div className='alert-description' dangerouslySetInnerHTML={{
+                __html: `Working with the head pro at one of the most popular courses in San Diego, we found that there were nearly 60 cancellations on average per DAY!!
+           
+           Most golf courses don’t penalize you for cancelling as long as you let them know 24-48 hours in advance, so golfers take advantage of this policy by proactively booking but cancelling the week of as their plans change.
+        
+           When looking at the data, we found that 50% of the time it was just one spot canceling, 20% of the time it was 2 slots, 3% of the time it was 3 slots and 27% of the time the entire foursome canceled.
+`.replace(/\n/g, "<br />")
+              }} />
+            </div>
+          </div>
+        </div>
+        <div style={{ height: '50px', background: "white", borderBottomRightRadius: "100%", borderBottomLeftRadius: '100%' }} ></div>
+
+      </div>
+
+      <div style={{ marginBlock: '100px' }}></div>
+      <Footer />
       <div style={{ marginBlock: '100px' }}></div>
 
-      <div style={{ display: 'flex', width: '90%', maxWidth: '1200px', margin: 'auto' }} >
-        <div style={{ width: '90%', maxWidth: '700px', margin: 'auto' }} >
-
-
-          <div className=' alert-title ' style={{ width: 'fit-content', margin: 'auto' }} > Our Story </div>
-          <div className='alert-description text-center'  >Tee Time Alerts was born out of our own frustration with the tee times booking process. We hope you find this tool as helpful as we have. Our golf game has thanked us for it.</div>
-          <button
-            type="button"
-            data-mdb-ripple="true"
-            data-mdb-ripple-color="light"
-            className="inline-block px-6 py-2.5  text-white font-medium text-xs leading-tight bg-green-500 uppercase rounded  hover:bg-green-600 hover:shadow-lg  focus:shadow-lg 
-          focus:outline-none focus:ring-0  active:shadow-lg transition duration-150 ease-in-out  hero-button-mobile"
-            style={{ display: 'flex', margin: 'auto', padding: '14px 28px', width: '202px', height: '52px', borderRadius: '12px', color: '#F5F8FD', fontSize: '16px', fontWeight: '700', lineHeight: '150%', marginTop: '25px' }}
-          >SET UP ALERT NOW</button>
-        </div>
-        <div>
-          <img src={GolfCartShadow} alt="hero" className='cart-shadow'  />
-          <img src={GolfCart} alt="hero" className='cart' />
-
-        </div>
-      </div>
-
-      <div style={{ width: '100%', height: 'fit-content', paddingBlock:'80px 20px', marginTop: '210px', background: 'rgba(3, 49, 75, 1)' }} >
-        <div className='footer-wrapper' >
-          <div className='footer-column' >
-            <img src={Logo} alt="logo" className="pr-2 " style={{ height: '55px', marginBottom:'20px' }} />
-            <div className='footer-slogan' >More golfing. Less planning.</div>
-          </div>
-
-          <div className='footer-column'>
-            <div className='footer-links-title'  >Quick Links</div>
-            <div>
-              <div className='footer-link' >Home </div>
-              <div className='footer-link' >Alerts </div>
-              <div className='footer-link' >Work </div>
-              <div className='footer-link' >Quick Tips </div>
-              <div className='footer-link' >Testimonial </div>
-              <div className='footer-link' >Case Study </div>
-              <div className='footer-link' >Our Story </div>
-            </div>
-          </div>
-          <div className='footer-column'>
-            <div className='footer-links-title'  >Contact Us</div>
-            <div>
-              <div className='footer-link' > Address: 4455 Landing Lange, APT 4 Louisville, KY 40018 </div>
-              <div className='footer-link' >Phone: (666) 478-9530 </div>
-              <div className='footer-link' >Email: hi@teatimealerts.co </div>
-
-            </div>
-          </div>
-          <div className='footer-column'>
-            <div className='footer-links-title'  >Get Social</div>
-            <div>
-              <div className='footer-link' >Instagram </div>
-              <div className='footer-link' >Facebook </div>
-              <div className='footer-link' >Twitter </div>
-            </div>
-          </div>
-
-        </div>
-        <div className='footer-link' style={{fontSize:'14px', width:'fit-content', margin:'auto', paddingTop:'30px'}} >Copyright ©  2023 TEATIME ALERTS </div>
-      </div>
 
       {/* <div style={{ paddingBottom: '200px' }} ></div> */}
     </div>
